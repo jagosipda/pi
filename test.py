a1fab3910976fd833bdd41cde937122e046f7443
jagosipda <125917117+jagosipda@users.noreply.github.com>
2023-03-06 17:42:38 +0900

    test

--- test.py
+++ test.py
@@ -4,3 +4,5 @@ my_str = url.replace("http://", "")  #규칙1
 my_str = my_str[:my_str.index(".")] #규칙2
 passward = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"  #규칙3
 print("{} 의 비밀번호는 {} 입니다.".format(url, passward))
+
+print("고양이는\n귀엽다")
