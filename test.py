0d22e922e8d962f9125b267bb6b5a9ae7b7604b1
jagosipda <125917117+jagosipda@users.noreply.github.com>
2023-03-09 23:00:39 +0900

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-url = "http://naver.com"
-my_str = url.replace("http://", "")  #규칙1 
-#print(my_str)
-my_str = my_str[:my_str.index(".")] #규칙2
-passward = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"  #규칙3
-print("{} 의 비밀번호는 {} 입니다.".format(url, passward))
